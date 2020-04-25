600b7f1329bd33d462bb9d41d426e0d9a8ad9a86
Andrew Reilley <areill1337@gmail.com>
2020-04-25 01:24:57 -0400

    Add methods for operating on a config directory

--- pyFileIO/__init__.py
+++ pyFileIO/__init__.py
@@ -1,4 +1,6 @@
+import os
 import yaml
+import appdirs
 
 from .util import isSemVerTuple, futureVersion
 
@@ -6,28 +8,37 @@ appVersion = None
 
 class FileIO():
     def __init__(self):
-        self.appVersion = None
-        self.fileTypes = []
-        self.migrations = {}
+        self._appName = None
+        self._appVersion = None
+        self._devMode = False
+        self._fileTypes = []
+        self._migrations = {}
 
-    def setAppVersion(self, version):
-        """Sets the internal version used as the end point for migrations"""
-        self.appVersion = version
+    def setAppInfo(self, name, version, devMode=False):
+        """Sets the name and internal version used as the end point for migrations"""
+        self._appName = name
+        self._appVersion = version
+        self._devMode = devMode
 
     def getAppVersion(self):
-        if self.appVersion is None:
+        if self._appVersion is None:
             raise ValueError('App version not set')
-        return self.appVersion
+        return self._appVersion
+
+    def getAppName(self):
+        if self._appName is None:
+            raise ValueError('App name not set')
+        return self._appName
 
     def registerFileType(self, name):
-        if name in self.fileTypes:
+        if name in self._fileTypes:
             raise ValueError('File type {} already exists'.format(name))
-        self.fileTypes.append(name)
-        self.migrations[name] = {}
+        self._fileTypes.append(name)
+        self._migrations[name] = {}
 
     def registerMigration(self, fileType, fromVer, toVer, func):
-        if fileType not in self.fileTypes:
-            raise TypeError('File type "{}" is not one of the known file types ({})'.format(fileType, self.fileTypes))
+        if fileType not in self._fileTypes:
+            raise TypeError('File type "{}" is not one of the known file types {}'.format(fileType, self._fileTypes))
         if not isSemVerTuple(fromVer):
             raise TypeError('Start version is not a semantic version tuple: {}'.format(fromVer))
         if not isSemVerTuple(toVer):
@@ -36,14 +47,14 @@ class FileIO():
             raise ValueError('Start version and end version equal')
         if not futureVersion(toVer, fromVer):
             raise ValueError('End version {} must be newer than start version {}'.format(toVer, fromVer))
-        self.migrations[fileType][fromVer] = {
+        self._migrations[fileType][fromVer] = {
             'to': toVer,
             'function': func
         }
 
     def migrateData(self, fileType, fromVer, toVer, data):
         currentVersion = fromVer
-        migrations = self.migrations[fileType]
+        migrations = self._migrations[fileType]
         while currentVersion != toVer:
             if currentVersion not in migrations:
                 raise ValueError('No migrations from {}'.format(currentVersion))
@@ -58,7 +69,7 @@ class FileIO():
         return data
 
     def save(self, fileType, data, path):
-        if fileType not in self.fileTypes:
+        if fileType not in self._fileTypes:
             raise ValueError('Unknown file type: {}'.format(fileType))
 
         output = {
@@ -71,7 +82,7 @@ class FileIO():
             yaml.dump(output, saveLocation)
 
     def load(self, fileType, path):
-        if fileType not in self.fileTypes:
+        if fileType not in self._fileTypes:
             raise ValueError('Unknown file type: {}'.format(fileType))
 
         with open(path, 'r') as readLocation:
@@ -90,10 +101,27 @@ class FileIO():
             if futureVersion(fileData['version'], self.getAppVersion()):
                 new = '.'.join(str(num) for num in fileData['version'])
                 old = '.'.join(str(num) for num in appVersion)
-                raise ValueError("Data is from a future version (" + new + " vs " + old + ") and can't be loaded.")
+                raise ValueError("Data is from a future version ({} vs {}) and can't be loaded.".format(new, old))
 
             # Otherwise it is from a past version and will be migrated
             return self.migrateData(fileType, fileData['version'], self.getAppVersion(), fileData['data'])
 
+    def getDataDirectory(self):
+        if self._devMode:
+            return ''
+        path = appdirs.user_data_dir(self.getAppName(), self.getAppName())
+        if not os.path.isdir(path): # Create directory if it doesn't exist
+            os.mkdir(path)
+        return '{}/'.format(path)
+
+    def saveToConfigDirectory(self, fileType, data, fileName):
+        if self._appName is None:
+            raise ValueError('Application name not set')
+        self.save(fileType, data, '{}{}'.format(self.getDataDirectory(), fileName))
+
+    def loadFromConfigDirectory(self, fileType, fileName):
+        if self._appName is None:
+            raise ValueError('Application name not set')
+        return self.load(fileType, '{}{}'.format(self.getDataDirectory(), fileName))
 
 fileIO = FileIO()
